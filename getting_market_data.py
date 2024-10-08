83e0b03f6058d81b8f666888bc9c868f1c9bc970
shantanu <shantanu.patra@webspiders.com>
2024-10-08 14:16:47 +0530

    getting NSDL Data

--- getting_market_data.py
+++ getting_market_data.py
@@ -12,20 +12,38 @@ def get_all_indices():
     for idx in all_indices['data']:
        nifty_index_data += "{} - {}".format(idx['index'], idx['last'])+"\n"
        #print("{} - {}".format(idx['index'], idx['last']))
-    return nifty_index_data 
+    return all_indices 
 
 def generate_html_email_body():
     today_date = datetime.now().strftime('%Y-%m-%d')
 
-    nifty_index_data = get_all_indices()
+    all_indices = get_all_indices()
     
     # Create the HTML email body with placeholders
     email_body = f"""
     <html>
     <body>
         <h2>Daily Report for {today_date}</h2>
-       <p><strong>Summary:</strong> {nifty_index_data}</p>
-       
+        <h3>Report Details:</h3>
+        <table border="1" cellpadding="5" cellspacing="0" style="border-collapse: collapse; width: 100%;">
+            <thead>
+                <tr>
+                    <th>#</th>
+                    <th>Detail</th>
+                </tr>
+            </thead>
+            <tbody>
+    """
+    for idx in all_indices['data']:
+        email_body += f"""
+                <tr>
+                    <td>{idx['index']}</td>
+                    <td>{idx['last']}</td>
+                </tr>
+        """
+    email_body += """
+            </tbody>
+        </table>
         <p>Best regards,<br>Your Python Automation Script</p>
     </body>
     </html>
@@ -39,7 +57,7 @@ def generate_html_email_body():
     #nifty_fut = get_nse_data() 
     #print(nifty_fut.head())
     #nifty_index_data = get_all_indices()
-  #  email_body = generate_html_email_body()
-  #  print(email_body)
+    #email_body = generate_html_email_body()
+   # print(email_body)
     #print(nifty_index_data)
   
